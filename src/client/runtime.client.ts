21828b83a80bac0bcf5dd3d12e21777731605fd2
sqzee1 <sqzgithub@gmail.com>
2025-06-25 01:42:22 -0300

    Initial commit

---
+++ src/client/runtime.client.ts
@@ -0,0 +1,11 @@
+import { Flamework } from "@flamework/core";
+
+Flamework.addPaths("src/client/components");
+Flamework.addPaths("src/client/controllers");
+Flamework.addPaths("src/client/manager");
+Flamework.addPaths("src/client/ui");
+Flamework.addPaths("src/shared/components");
+Flamework.addPaths("src/shared/hooks");
+Flamework.addPaths("src/shared/utils");
+
+Flamework.ignite();
